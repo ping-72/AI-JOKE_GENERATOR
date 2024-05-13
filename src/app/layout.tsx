aacbf8943eb97d11a66ca7bb934370aca6cf6e27
Prince <kumarprince551178@gmai.com>
2024-05-13 21:06:59 +0530

    files added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        <footer className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+          <div className="fixed bottom-0 right-0 mb-32 z-50 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+            <a
+              href="https://github.com/ping-72"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Developed by Prince{" "}
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Find Sourcecode on Github
+              </p>
+            </a>
+          </div>
+        </footer>
+      </body>
     </html>
+    </>
   );
 }
